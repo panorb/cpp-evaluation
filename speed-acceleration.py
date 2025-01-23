f2778395cad27aa9dc79c24d645b5c9185a13c32
Paul Norberger <hi@paulnorberger.de>
2025-01-23 15:52:52 +0100

    Speed acceleration graph

--- speed-acceleration.py
+++ speed-acceleration.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-trace_file_name = "C:/Users/panor/OneDrive/Dokumente/Masterarbeit/Traces/PAULMANUSPARKNORM.csv"
+trace_file_name = "D:/OneDrive/Dokumente/Masterarbeit/Traces/PAULMANUSPARKNORM.csv"
 
 mapping_names = {
     "/Channel/!SEGA/vaIbc [u1  1]": "X",
@@ -43,47 +43,73 @@ with open(trace_file_name, encoding="iso-8859-1") as trace_file:
 
     trace_df = pd.read_csv(trace_file_name, index_col=False, encoding="iso-8859-1", sep=',', skiprows=line_num + 1, names=headings, dtype=trace_dtypes)
 
-# Berechnungen
-trace_df['Geschwindigkeit_X'] = np.diff(trace_df['X']) / np.diff(trace_df['Time'])
-trace_df['Geschwindigkeit_Y'] = np.diff(trace_df['Y']) / np.diff(trace_df['Time'])
-trace_df['Geschwindigkeit_Z'] = np.diff(trace_df['Z']) / np.diff(trace_df['Time'])
+trace_df = trace_df[trace_df["LineNumber"] > 1]
 
-trace_df['Beschleunigung_X'] = np.diff(trace_df['Geschwindigkeit_X']) / np.diff(trace_df['Time'])
-trace_df['Beschleunigung_Y'] = np.diff(trace_df['Geschwindigkeit_Y']) / np.diff(trace_df['Time'])
-trace_df['Beschleunigung_Z'] = np.diff(trace_df['Geschwindigkeit_Z']) / np.diff(trace_df['Time'])
+# Berechne Geschwindigkeiten
+trace_df['Vx'] = trace_df['X'].diff() / 0.004  # 4ms pro Tick in Sekunden
+trace_df['Vy'] = trace_df['Y'].diff() / 0.004
+trace_df['Vz'] = trace_df['Z'].diff() / 0.004
+trace_df['Ges'] = np.sqrt(trace_df['Vx']**2 + 
+                                     trace_df['Vy']**2 + trace_df['Vz'])
+    # trace_df['Vz']**2)
 
-trace_df['Ruck_X'] = np.diff(trace_df['Beschleunigung_X']) / np.diff(trace_df['Time'])[1:]
-trace_df['Ruck_Y'] = np.diff(trace_df['Beschleunigung_Y']) / np.diff(trace_df['Time'])[1:]
-trace_df['Ruck_Z'] = np.diff(trace_df['Beschleunigung_Z']) / np.diff(trace_df['Time'])[1:]
+# Berechne Beschleunigungen
+trace_df['Ax'] = trace_df['Vx'].diff() / 0.004
+trace_df['Ay'] = trace_df['Vy'].diff() / 0.004
+trace_df['Az'] = trace_df['Vz'].diff() / 0.004
+trace_df['Bes'] = trace_df['Ges'].diff() / 0.004 
 
-# Plotting
-plt.figure(figsize=(12, 8))
+# Berechne Ruck (JERK)
+trace_df['Jx'] = trace_df['Ax'].diff() / 0.004
+trace_df['Jy'] = trace_df['Ay'].diff() / 0.004
+trace_df['Jz'] = trace_df['Az'].diff() / 0.004
+trace_df['Ruc'] = trace_df['Bes'].diff() / 0.004
 
-plt.subplot(3, 1, 1)
-plt.plot(trace_df['Time'][1:], trace_df['Geschwindigkeit_X'], label='Geschwindigkeit X')
-plt.plot(trace_df['Time'][1:], trace_df['Geschwindigkeit_Y'], label='Geschwindigkeit Y')
-plt.plot(trace_df['Time'][1:], trace_df['Geschwindigkeit_Z'], label='Geschwindigkeit Z')
-plt.xlabel('Zeit')
-plt.ylabel('Geschwindigkeit')
-plt.legend()
+# Fülle fehlende Werte (z.B. in der ersten Zeile) mit 0
+# trace_df.fillna(0, inplace=True)
 
-plt.subplot(3, 1, 2)
-plt.plot(trace_df['Time'][2:], trace_df['Beschleunigung_X'], label='Beschleunigung X')
-plt.plot(trace_df['Time'][2:], trace_df['Beschleunigung_Y'], label='Beschleunigung Y')
-plt.plot(trace_df['Time'][2:], trace_df['Beschleunigung_Z'], label='Beschleunigung Z')
-plt.xlabel('Zeit')
-plt.ylabel('Beschleunigung')
-plt.legend()
+# Subplots erstellen
+fig, axes = plt.subplots(nrows=4, ncols=1, figsize=(10, 15))
 
-plt.subplot(3, 1, 3)
-plt.plot(trace_df['Time'][3:], trace_df['Ruck_X'], label='Ruck X')
-plt.plot(trace_df['Time'][3:], trace_df['Ruck_Y'], label='Ruck Y')
-plt.plot(trace_df['Time'][3:], trace_df['Ruck_Z'], label='Ruck Z')
-plt.xlabel('Zeit')
-plt.ylabel('Ruck')
-plt.legend()
+# x-Werte plotten
+axes[0].plot(trace_df['Time'], trace_df['X'], label='X')
+axes[0].set_ylabel('X [m]')
+axes[0].legend()
 
+# y-Werte plotten
+axes[1].plot(trace_df['Time'], trace_df['Y'], label='Y')
+axes[1].set_ylabel('Y [m]')
+axes[1].legend()
+
+# z-Werte plotten
+axes[2].plot(trace_df['Time'], trace_df['Z'], label='Z')
+axes[2].set_ylabel('Z [m]')
+axes[2].legend()
+
+# Geschwindigkeit plotten
+axes[3].plot(trace_df['Time'], trace_df['Ges'], label='Geschwindigkeit')
+axes[3].set_ylabel('Geschwindigkeit [m/s]')
+axes[3].legend()
+
+
+trace_df = trace_df[trace_df['Ges'] > 0]
+# Beschleunigung plotten
+# axes[4].plot(trace_df['Time'], trace_df['Bes'], label='Beschleunigung')
+# axes[4].set_ylabel('Beschleunigung [m/s^2]')
+# axes[4].legend()
+# 
+# # Ruck plotten
+# axes[5].plot(trace_df['Time'], trace_df['Ruc'], label='Ruck')
+# axes[5].set_ylabel('Ruck [m/s^3]')
+# axes[5].legend()
+
+# Allgemeine Einstellungen für alle Subplots
+for ax in axes:
+    ax.set_xlabel('Zeit [s]')
+    ax.grid(True)
+
+# Layout anpassen
 plt.tight_layout()
-plt.show()
 
-print(trace_df)
+# Plot anzeigen
+plt.show()
